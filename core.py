028c921af9f5f30cde7c64dee0591e111b9b5fc9
PvonK <philippvonkesselstatt@gmail.com>
2025-06-06 16:43:21 -0300

    refactored find_all_files function

--- core.py
+++ core.py
@@ -1,7 +1,6 @@
 # core.py
 
 import os
-import subprocess
 from .Book import Book
 from .formatter import progress_bar
 from .file_sorter import move_book
@@ -12,13 +11,13 @@ from .file_sorter import move_book
 # (or the metadata found gets close but not quite) like:
 
 
-def find_find_all_files(find_dir):
-    result = subprocess.run(
-        ['find', find_dir, '-type', 'f'],
-        stdout=subprocess.PIPE)
-    result = result.stdout.decode("utf-8").split('\n')
-    result.pop()
-    return result
+def find_all_files(find_dir):
+    all_files = []
+    for dirpath, _, filenames in os.walk(find_dir):
+        for file in filenames:
+            full_path = os.path.join(dirpath, file)
+            all_files.append(full_path)
+    return all_files
 
 
 # find all the categories defined in a list of books
@@ -57,18 +56,21 @@ def organize_dir(
         book.organize_book()
         return book
 
-    all_files = find_find_all_files(directory)
+    all_files = find_all_files(directory)
     for n in range(len(all_files)):
         file = all_files[n]
         if os.path.isfile(file):
+
             ext = os.path.splitext(file)[1]
             if not is_ext_valid(ext):
                 continue
+
             book = Book(
                 file,
                 interactive_organizer=interactive,
                 output_path_dir=output_path_dir
                 )
             book.organize_book()
-            move_book(book, output, dry)
+            move_book(book, dry)
+
         progress_bar(len(all_files), n)
